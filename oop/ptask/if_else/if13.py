c5f42ba0dd2449a37b234243ba4b7678aeb8fd9f
Netitoko <netitokoo@gmail.com>
2025-09-29 00:39:36 +0300

    task

---
+++ oop/ptask/if_else/if13.py
@@ -0,0 +1,16 @@
+'''If13. Даны три числа. Найти среднее из них
+(т. е. число, расположенное между наименьшим и наибольшим).'''
+
+# Найти среднее из трех чисел
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a <= b <= c or c <= b <= a:
+    middle = b
+elif b <= a <= c or c <= a <= b:
+    middle = a
+else:
+    middle = c
+
+print(f"Среднее число: {middle}")
